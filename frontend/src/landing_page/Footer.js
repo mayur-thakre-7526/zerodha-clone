60e0fd388059b30f693e9932868a68778740e785
Mayur Thakre <mayurthakre878@gmail.com>
2026-08-17 16:17:39 +0530

    Completed Home page implementation

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,257 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (  
-        <h1>Footer</h1>
-    );
+  return (
+    <footer
+      className="mt-5 pt-5 pb-5 border-top"
+      style={{ backgroundColor: "rgb(250,250,250)" }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img
+              src="media/images/logo.svg"
+              alt="Zerodha Logo"
+              style={{ width: "150px" }}
+              className="mb-3"
+            />
+
+            <p>&copy; 2010 - 2026, Zerodha Broking Ltd. All rights reserved.</p>
+          </div>
+
+          <div className="col">
+            <p className="fs-5">Account</p>
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Open demat account
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Minor demat account
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              NRI demat account
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              HUF demat account
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Commodity
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Dematerialisation
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Fund transfer
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              MTF
+            </a>
+            <br /> <br />
+          </div>
+
+          <div className="col">
+            <p className="fs-5">Support</p>
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Contact us
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Support portal
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              How to file a complaint?
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Status of your complaints
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Bulletin
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Circular
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Z-Connect blog
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Downloads
+            </a>
+            <br /> <br />
+          </div>
+
+          <div className="col">
+            <p className="fs-5">Company</p>
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              About
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Philosophy
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Press & media
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Careers
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Zerodha Cares (CSR)
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Zerodha.tech
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Open source
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Referral program
+            </a>
+            <br /> <br />
+          </div>
+
+          <div className="col">
+            <p className="fs-5">Quick links</p>
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Upcoming IPOs
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Brokerage charges
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Market holidays
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Economic calendar
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Calculators
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Markets
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Sectors
+            </a>
+            <br /> <br />
+            <a href="#" style={{ textDecoration: "none", color: "#6C6B6A" }}>
+              Gift Nifty
+            </a>
+            <br /> <br />
+          </div>
+        </div>
+
+        <div className="mt-5 text-muted" style={{ fontSize: "0.8rem" }}>
+          <p>
+            Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI
+            Registration no.: INZ000031633 CDSL/NSDL: Depository services
+            through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019
+            Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross,
+            Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase,
+            Bengaluru - 560078, Karnataka, India. For any complaints pertaining
+            to securities broking please write to complaints@zerodha.com, for DP
+            related to dp@zerodha.com. Please ensure you carefully read the Risk
+            Disclosure Document as prescribed by SEBI | ICF
+          </p>
+          <br />
+
+          <p>
+            Procedure to file a complaint on SEBI SCORES: Register on SCORES
+            portal. Mandatory details for filing complaints on SCORES: Name,
+            PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+            Communication, Speedy redressal of the grievances
+          </p>
+          <br />
+
+          <p>
+            Attention investors: 1) Stock brokers can accept securities as
+            margins from clients only by way of pledge in the depository system
+            w.e.f September 01, 2020. 2) Update your e-mail and phone number
+            with your stock broker / depository participant and receive OTP
+            directly from depository on your e-mail and/or mobile number to
+            create pledge. 3) Check your securities / MF / bonds in the
+            consolidated account statement issued by NSDL/CDSL every month.
+          </p>
+
+          <br />
+
+          <p>
+            Fixed deposit products offered on this platform are third-party
+            products (TPP) and are not Exchange traded products. These are
+            offered through Blostem Fintech Private Limited. Zerodha Broking
+            Limited (SEBI Registration No.: INZ000031633) is acting solely as a
+            distributor for these products. Any disputes arising with respect to
+            such distribution activity will not have access to SEBI SCORES/ODR,
+            Exchange Investor Grievance Redressal Forum, or Arbitration
+            mechanism. Fixed deposits are regulated by the Reserve Bank of India
+            (RBI).
+          </p>
+        </div>
+
+        <div
+          style={{
+            fontSize: "0.8rem",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            NSE
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            BSE
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            MCX
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            MSEI
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            Terms & conditions
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            Policies & procedures
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            Privacy policy
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            Disclosure
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            For investor's attention
+          </a>
+          <a href="#" style={{ textDecoration: "none", color: "#6C6B6A"}}>
+            Investor charter
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -11,22 +11,26 @@ function Pricing() {
             in India. Flat fees and no hidden charges.
           </p>
           <a href="" style={{ textDecoration: "none" }}>
-          See pricing <i class="fa-solid fa-arrow-right-long"></i>
-        </a>
-
+            See pricing <i class="fa-solid fa-arrow-right-long"></i>
+          </a>
         </div>
 
         <div className="col-2"></div>
         <div className="col-6 mb-5">
           <div className="row text-center">
-
             <div className="col border p-3">
-              <h1 className="mb-4"><i class="fa-solid fa-indian-rupee-sign fs-2"></i>0</h1>
-              <p>Free equity delivery and <br/> direct mutual funds</p>
+              <h1 className="mb-4">
+                <i class="fa-solid fa-indian-rupee-sign fs-2"></i>0
+              </h1>
+              <p>
+                Free equity delivery and <br /> direct mutual funds
+              </p>
             </div>
 
             <div className="col border p-3">
-              <h1 className="mb-4"><i class="fa-solid fa-indian-rupee-sign fs-2"></i>20</h1>
+              <h1 className="mb-4">
+                <i class="fa-solid fa-indian-rupee-sign fs-2"></i>20
+              </h1>
               <p>Intraday and F&O</p>
             </div>
           </div>
