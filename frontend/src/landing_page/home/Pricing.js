c49ecfdd1b01a601c22a9e8656f0dd4d2cd2f6ce
Mayur Thakre <mayurthakre878@gmail.com>
2026-08-17 13:32:09 +0530

    Implement Unbeatable pricing and Free and open market education sections

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,7 +1,39 @@
 import React from "react";
 
-function Education() {
-  return <h1>Education</h1>;
+function Pricing() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h1 className="mb-3 fs-2">Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+          See pricing <i class="fa-solid fa-arrow-right-long"></i>
+        </a>
+
+        </div>
+
+        <div className="col-2"></div>
+        <div className="col-6 mb-5">
+          <div className="row text-center">
+
+            <div className="col border p-3">
+              <h1 className="mb-4"><i class="fa-solid fa-indian-rupee-sign fs-2"></i>0</h1>
+              <p>Free equity delivery and <br/> direct mutual funds</p>
+            </div>
+
+            <div className="col border p-3">
+              <h1 className="mb-4"><i class="fa-solid fa-indian-rupee-sign fs-2"></i>20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Education;
+export default Pricing;
