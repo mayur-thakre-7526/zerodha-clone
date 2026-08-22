46f1a99eef2bec790073d4660cf5e32992f84231
Mayur Thakre <mayurthakre878@gmail.com>
2026-08-22 19:25:06 +0530

    Implement the Holdings Page Table  Data

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { holdings } from "../data/data";
 
 const Holdings = () => {
   return (
@@ -17,6 +18,28 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {holdings.map((stock, idx) => {
+            const curValue = stock.price * stock.qty;
+            const isProfit = curValue - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isLoss ? "loss" : "profit";
+
+            return (
+              <tr>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{curValue}</td>
+                <td className={profClass}>
+                  {(curValue - stock.avg * stock.qty).toFixed(2)}
+                </td>
+                <td className={profClass}>{stock.net}</td>
+                <td className={dayClass}>{stock.day}</td>
+              </tr>
+            );
+          })}
         </table>
       </div>
 
